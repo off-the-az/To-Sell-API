f3eecbee8ca4b23c0c7265d340760636c9ac519e
off-the-az <0ff.th3.az@gmail.com>
2023-08-02 16:49:30 +0300

    Create Categories Entity

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,1 +1,9 @@
-export class Category {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'category' })
+export class Category {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column({length: 255})
+    name: string;
+}
